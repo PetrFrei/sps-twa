e374ea72c34d06c5ecf6d3c6bf7462c5b434d720
Petr Frei <petrfrei194@gmail.com>
2018-01-22 14:50:35 +0100

    Vytvori entitni tridy

---
+++ src/EshopBundle/Entity/Product.php
@@ -0,0 +1,56 @@
+<?php
+namespace EshopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Product
+ *
+ * @author freipe
+ * @ORM\Entity()
+ */
+class Product {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $name;
+    /**
+     * @ORM\Column(type="string", length=128, unique=true)
+     */
+    private $url;
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Popis;
+    
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+
+}
